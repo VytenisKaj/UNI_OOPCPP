62de7a1b987f7f898b16377f82df13692498ef4a
VytenisKaj <79807005+VytenisKaj@users.noreply.github.com>
2021-03-06 19:24:33 +0200

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,24 +1,92 @@
 #include"Car.h"
+#include<sstream>
+#include<exception>
+#include<stdexcept>
 
 unsigned int MyCar::Car::ID = 0;
+const std::string MyCar::CarCheck::possibleColors[] = {
 
-MyCar::Car::Car(){
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Black",
+        "White",
+        "Grey"
 
-    this->color = DEFAULT_COLOR;
-    this->vin = DEFAULT_VIN;
-    this->plate = DEFAULT_PLATE;
-    this->enginePower = DEFAULT_POWER;
-    this->model = DEFAULT_MODEL;
-    this->weight = DEFAULT_WEIGHT;
-    this->manufacturer = DEFAULT_MANUFACTURER;
-    this->manufactureYear = DEFAULT_MANUFACTURE_YEAR;
-    ++ID;
-    objectID = ID;
+};
+
+
+// Checkers
+
+bool MyCar::CarCheck::checkColor(const std::string& color){
+
+    for(int i = 0; i < COLOR_AMOUNT; ++i){
+        if(color == possibleColors[i]){
+            return true;
+        }
+    }
+    return false;
+
 }
 
-MyCar::Car::Car(const std::string manufacturer, const std::string model, const unsigned int manufactureYear, const std::string color,
-                const std::string plate, const std::string vin, const unsigned int enginePower, const unsigned int weight)
+bool MyCar::CarCheck::checkPlate(const std::string& plate){
+
+    for(int i = 0; i < (int) plate.length(); ++i){
+        if(i < LETTER_AMOUNT){
+            if(!isupper(plate[i])){
+                return false;
+            }
+        }
+        else{
+            if(!isdigit(plate[i])){
+                return false;
+            }
+        }
+        if(i > PLATE_LENGTH){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool MyCar::CarCheck::checkEnginePower(const unsigned int& enginePower){
+
+    if(enginePower <= MAX_ENGINE_POWER){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+bool MyCar::CarCheck::checkWeight(const unsigned int& weight){
+
+    if(weight <= MAX_WEIGHT){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool MyCar::CarCheck::checkManufactureYear(const unsigned int& manufactureYear){
+
+    if(manufactureYear >= FIRST_CAR_DATE && manufactureYear <= CURRENT_YEAR){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+MyCar::Car::Car(const std::string& manufacturer, const std::string& model, const unsigned int& manufactureYear, const std::string& color,
+                const std::string& plate, const std::string& vin, const unsigned int& enginePower, const unsigned int& weight)
 {
+
     setColor(color);
     setVin(vin);
     setPlate(plate);
@@ -34,6 +102,8 @@ MyCar::Car::Car(const std::string manufacturer, const std::string model, const u
 MyCar::Car::~Car(){
     // empty
 }
+
+
 std::string MyCar::Car::toString(){
 
     std::stringstream ss;
@@ -76,10 +146,14 @@ unsigned int MyCar::Car::getManufactureYear(){
     return manufactureYear;
 }
 
+unsigned int MyCar::Car::getID(){
+    return objectID;
+}
+
 // Setters
 
-void MyCar::Car::setColor(const std::string color){
-    if(checkColor(color)){
+void MyCar::Car::setColor(const std::string& color){
+    if(check.checkColor(color)){
         this->color = color;
     }
     else{
@@ -87,21 +161,21 @@ void MyCar::Car::setColor(const std::string color){
     }
 }
 
-void MyCar::Car::setVin(const std::string vin){
+void MyCar::Car::setVin(const std::string& vin){
     this->vin = vin;
 }
 
-void MyCar::Car::setPlate(const std::string plate){
-    if(checkPlate(plate)){
+void MyCar::Car::setPlate(const std::string& plate){
+    if(check.checkPlate(plate)){
         this->plate = plate;
     }
     else{
-        throw std::invalid_argument("Not available plate");
+        throw std::invalid_argument("Not a valid plate (plate has to have only 3 capital letters followed by 3 numbers)");
     }
 }
 
-void MyCar::Car::setEnginePower(const unsigned int enginePower){
-    if(checkEnginePower(enginePower)){
+void MyCar::Car::setEnginePower(const unsigned int& enginePower){
+    if(check.checkEnginePower(enginePower)){
         this->enginePower = enginePower;
     }
     else{
@@ -109,8 +183,8 @@ void MyCar::Car::setEnginePower(const unsigned int enginePower){
     }
 
 }
-void MyCar::Car::setWeight(const unsigned int weight){
-    if(checkWeight(weight)){
+void MyCar::Car::setWeight(const unsigned int& weight){
+    if(check.checkWeight(weight)){
         this->weight = weight;
     }
     else{
@@ -119,78 +193,19 @@ void MyCar::Car::setWeight(const unsigned int weight){
 
 }
 
-void MyCar::Car::setManufacturer(const std::string manufacturer){
+void MyCar::Car::setManufacturer(const std::string& manufacturer){
     this->manufacturer = manufacturer;
 }
 
-void MyCar::Car::setModel(const std::string model){
+void MyCar::Car::setModel(const std::string& model){
     this->model = model;
 }
 
-void MyCar::Car::setManufactureYear(const unsigned int manufactureYear){
-    if(checkManufactureYear(manufactureYear)){
+void MyCar::Car::setManufactureYear(const unsigned int& manufactureYear){
+    if(check.checkManufactureYear(manufactureYear)){
         this->manufactureYear = manufactureYear;
     }
     else{
         throw std::invalid_argument("Manufacture year out of range");
-    }
-}
-
-// Checkers
-
-bool MyCar::Car::checkColor(const std::string color){
-    for(int i = 0; i < COLOR_AMOUNT; ++i){
-        if(color == possibleColors[i]){
-            return true;
-        }
-    }
-    return false;
-
-}
-
-bool MyCar::Car::checkPlate(const std::string plate){
-    for(int i = 0; i < plate.length(); ++i){
-        if(i < LETTER_AMOUNT){
-            if(!isupper(plate[i])){
-                return false;
-            }
-        }
-        else{
-            if(!isdigit(plate[i])){
-                return false;
-            }
-        }
-        if(i > NUMBER_LENGTH){
-            return false;
-        }
-    }
-    return true;
-}
-
-bool MyCar::Car::checkEnginePower(const unsigned int enginePower){
-    if(enginePower <= MAX_ENGINE_POWER){
-        return true;
-    }
-    else{
-        return false;
-    }
-
-}
-
-bool MyCar::Car::checkWeight(const unsigned int weight){
-    if(weight <= MAX_WEIGHT){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-bool MyCar::Car::checkManufactureYear(const unsigned int manufactureYear){
-    if(manufactureYear >= FIRST_CAR_DATE && manufactureYear <= CURRENT_YEAR){
-        return true;
-    }
-    else{
-        return false;
     }
 }
