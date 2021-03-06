db2bee55022a3cff82928970462ec2f6c13ce132
VytenisKaj <79807005+VytenisKaj@users.noreply.github.com>
2021-03-06 19:27:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
     try{
-        Car car1;
+        Car car1("Porsche","Panamera",2020,"Black","CCC222","jbsajbc2211s",350,2000);
         Car car2("Subaru","Impreza",2013,"Grey","ABC123","JPKSKD12612",110,1950);
-        Car car3;
+
         Car car4("Audi","100",1998,"Red","AGH354","VWksadjasf222151",128,2500);
 
         cout << "Car 2 color " << car2.getColor() << endl;
@@ -20,28 +20,29 @@ int main()
         cout << "Car 4 plate " << car4.getPlate() << endl;
         cout << "Car 4 vin " << car4.getVin() << endl;
         cout << "Car 4 weight " << car4.getWeight() << " KG" << endl;
+        cout << "Car 4 ID " << car4.getID() << endl;
 
         cout << car1.toString() << endl;
         cout << car2.toString() << endl;
         cout << car4.toString() << endl;
-        car3.setManufacturer("Porsche");
-        car3.setColor("Black");
-        car3.setVin("jbsajbc2211s");
-        car3.setManufactureYear(2020);
-        car3.setEnginePower(350);
-        car3.setModel("Panamera");
-        car3.setWeight(2000);
-        cout << car3.toString() << endl;
-        car3.setPlate("bb222s22"); // throws exception
+        car1.setManufacturer("Ferrari");
+        car1.setColor("Red");
+        car1.setVin("bifhdksom256482");
+        car1.setManufactureYear(2018);
+        car1.setEnginePower(460);
+        car1.setModel("458");
+        car1.setWeight(1800);
+        cout << car1.toString() << endl; // ID has not changed, because car1 object did not change
+        car1.setPlate("bb222s22"); // throws exception
 
 
 
     }
-    catch(invalid_argument& except){
-        cerr << "Invalid argument: "<< except.what() << endl;
+    catch(invalid_argument& err){
+        cerr << "Invalid argument: "<< err.what() << endl;
     }
     catch(...){
-        cerr << "Exception detected" << endl;
+        cerr << "Unknown exception detected" << endl;
     }
     return 0;
 }
